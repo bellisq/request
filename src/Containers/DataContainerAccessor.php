cd601982496186072aef038406b150061a815f33
Showsay You <akizuki.c10.l65@gmail.com>
2018-01-30 16:22:11 +0900

    Update DataContainerAccessor

--- src/Containers/DataContainerAccessor.php
+++ src/Containers/DataContainerAccessor.php
@@ -6,6 +6,7 @@ use Closure;
 use DomainException;
 use Strict\Property\Utility\StrictPropertyContainer;
 use Strict\Property\Intermediate\PropertyRegister;
+use TypeError;
 
 
 /**
@@ -74,6 +75,42 @@ abstract class DataContainerAccessor
      * @param string           $propertyName
      * @param string           $containerKey
      */
+    final protected function registerNullableStringProperty(
+        PropertyRegister $propertyRegister,
+        string $propertyName,
+        string $containerKey
+    ): void {
+        $propertyRegister
+            ->newVirtualProperty(
+                $propertyName,
+                $this->generateNullableStringGetter($containerKey),
+                $this->generateNullableStringSetter($containerKey)
+            );
+    }
+
+    /**
+     * @param PropertyRegister $propertyRegister
+     * @param string           $propertyName
+     * @param string           $containerKey
+     */
+    final protected function registerReadonlyNullableStringProperty(
+        PropertyRegister $propertyRegister,
+        string $propertyName,
+        string $containerKey
+    ): void {
+        $propertyRegister
+            ->newVirtualProperty(
+                $propertyName,
+                $this->generateNullableStringGetter($containerKey),
+                null
+            );
+    }
+
+    /**
+     * @param PropertyRegister $propertyRegister
+     * @param string           $propertyName
+     * @param string           $containerKey
+     */
     final protected function registerIntProperty(
         PropertyRegister $propertyRegister,
         string $propertyName,
@@ -102,6 +139,91 @@ abstract class DataContainerAccessor
                 $propertyName,
                 $this->generateIntGetter($containerKey),
                 null
+            );
+    }
+
+    /**
+     * @param PropertyRegister $propertyRegister
+     * @param string           $propertyName
+     * @param string           $containerKey
+     */
+    final protected function registerNullableIntProperty(
+        PropertyRegister $propertyRegister,
+        string $propertyName,
+        string $containerKey
+    ): void {
+        $propertyRegister
+            ->newVirtualProperty(
+                $propertyName,
+                $this->generateNullableIntGetter($containerKey),
+                $this->generateNullableIntSetter($containerKey)
+            );
+    }
+
+    /**
+     * @param PropertyRegister $propertyRegister
+     * @param string           $propertyName
+     * @param string           $containerKey
+     */
+    final protected function registerReadonlyNullableIntProperty(
+        PropertyRegister $propertyRegister,
+        string $propertyName,
+        string $containerKey
+    ): void {
+        $propertyRegister
+            ->newVirtualProperty(
+                $propertyName,
+                $this->generateNullableIntGetter($containerKey),
+                null
+            );
+    }
+
+    /**
+     * @param PropertyRegister $propertyRegister
+     * @param string           $propertyName
+     * @param string           $containerKey
+     */
+    final protected function registerArrayProperty(
+        PropertyRegister $propertyRegister,
+        string $propertyName,
+        string $containerKey
+    ): void {
+        $propertyRegister
+            ->newVirtualProperty(
+                $propertyName,
+                function () use ($containerKey): MutableArray {
+                    return $this->dataContainer[$containerKey];
+                },
+                function ($value) use ($containerKey, $propertyName): void {
+                    if (is_array($value)) {
+                        $this->dataContainer[$containerKey] = new MutableArray($value);
+                    } else if ($value instanceof MutableArray) {
+                        $this->dataContainer[$containerKey] = $value;
+                    } else {
+                        throw new TypeError("Property \${$propertyName} must be of the type array or of the type MutableArray.");
+                    }
+                }
+            );
+    }
+
+    /**
+     * @param PropertyRegister $propertyRegister
+     * @param string           $propertyName
+     * @param string           $containerKey
+     */
+    final protected function registerReadonlyArrayProperty(
+        PropertyRegister $propertyRegister,
+        string $propertyName,
+        string $containerKey
+    ): void {
+        $propertyRegister
+            ->newVirtualProperty(
+                $propertyName,
+                function () use ($containerKey): array {
+                    /** @var $ma MutableArray */
+                    $ma = $this->dataContainer[$containerKey];
+                    return $ma->getArrayCopy();
+                },null
             );
     }
 
@@ -145,6 +267,51 @@ abstract class DataContainerAccessor
     final protected function generateIntSetter(string $containerKey): Closure
     {
         return function (int $value) use ($containerKey): void {
+            $this->dataContainer[$containerKey] = $value;
+        };
+    }
+
+
+    /**
+     * @param string $containerKey
+     * @return Closure
+     */
+    final protected function generateNullableStringGetter(string $containerKey): Closure
+    {
+        return function () use ($containerKey): ?string {
+            return $this->dataContainer[$containerKey];
+        };
+    }
+
+    /**
+     * @param string $containerKey
+     * @return Closure
+     */
+    final protected function generateNullableStringSetter(string $containerKey): Closure
+    {
+        return function (?string $value) use ($containerKey): void {
+            $this->dataContainer[$containerKey] = $value;
+        };
+    }
+
+    /**
+     * @param string $containerKey
+     * @return Closure
+     */
+    final protected function generateNullableIntGetter(string $containerKey): Closure
+    {
+        return function () use ($containerKey): ?int {
+            return $this->dataContainer[$containerKey];
+        };
+    }
+
+    /**
+     * @param string $containerKey
+     * @return Closure
+     */
+    final protected function generateNullableIntSetter(string $containerKey): Closure
+    {
+        return function (?int $value) use ($containerKey): void {
             $this->dataContainer[$containerKey] = $value;
         };
     }
