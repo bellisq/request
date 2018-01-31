fe509db5d869e7e304517cd042b5d62fa5c23f5b
Showsay You <akizuki.c10.l65@gmail.com>
2018-01-31 11:38:15 +0900

    Move constants

--- src/Containers/RequestDataContainer.php
+++ src/Containers/RequestDataContainer.php
@@ -2,6 +2,7 @@
 
 namespace Bellisq\Request\Containers;
 
+use Bellisq\Request\Constants\RequestConstantsInterface;
 use Bellisq\Request\Containers\MutableArray;
 
 
@@ -15,22 +16,8 @@ use Bellisq\Request\Containers\MutableArray;
  */
 class RequestDataContainer
     extends MutableArray
+    implements RequestConstantsInterface
 {
-    public const METHOD_GET    = 'GET';
-    public const METHOD_POST   = 'POST';
-    public const METHOD_PUT    = 'PUT';
-    public const METHOD_DELETE = 'DELETE';
-
-    public const PORT_HTTP = 80;
-    public const PORT_HTTPS = 443;
-
-    public const SCHEME_HTTP = 'http';
-    public const SCHEME_HTTPS = 'https';
-
-    public const PROTOCOL_HTTP10 = 'HTTP/1.0';
-    public const PROTOCOL_HTTP11 = 'HTTP/1.1';
-    public const PROTOCOL_HTTP20 = 'HTTP/2.0';
-
     public const VAR_LINE_SCHEME   = 'line.scheme';
     public const VAR_LINE_HOST     = 'line.host';
     public const VAR_LINE_PORT     = 'line.port';
